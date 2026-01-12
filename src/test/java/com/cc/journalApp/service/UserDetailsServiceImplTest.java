565a1c7255a1da07f63fc40e0d11b8509a2b2f5e
Varun Kumar <varunkr2109@gmail.com>
2026-01-13 03:20:26 +0530

    basic testing using Mockito

---
+++ src/test/java/com/cc/journalApp/service/UserDetailsServiceImplTest.java
@@ -0,0 +1,38 @@
+package com.cc.journalApp.service;
+
+import com.cc.journalApp.models.User;
+import com.cc.journalApp.repository.UserRepository;
+import com.cc.journalApp.request.UserRequest;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentMatchers;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.List;
+
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+public class UserDetailsServiceImplTest {
+
+    @InjectMocks
+    private UserDetailsServiceImpl userDetailsServiceImpl;
+    @Mock
+    private UserRepository userRepository;
+
+    @Test
+    void getUserByUserName() {
+        User u = new User(new UserRequest("abcd", "qwer"));
+        u.setRoles(List.of("USER"));
+        when(userRepository.findByUserName(ArgumentMatchers.anyString()))
+                .thenReturn(u);
+        UserDetails userDetails = userDetailsServiceImpl.loadUserByUsername("abcd");
+        Assertions.assertNotNull(userDetails);
+    }
+}
